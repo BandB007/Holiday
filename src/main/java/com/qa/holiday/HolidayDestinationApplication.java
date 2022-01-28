d0119115192b6e474b77d11ca0fbdf4b9eee950b
BanndB007 <bdstudy07@gmail.com>
2022-01-28 14:08:43 +0000

    Initial commit

---
+++ src/main/java/com/qa/holiday/HolidayDestinationApplication.java
@@ -0,0 +1,13 @@
+package com.qa.holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayDestinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayDestinationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chocloate/HolidayDestinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chocloate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayDestinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
